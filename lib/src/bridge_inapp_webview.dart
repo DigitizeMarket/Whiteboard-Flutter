5aefedce44141ba48e1fc0590a8f21e0370bb056
Jun Ying <jying@simplec.com>
2022-06-14 00:11:33 -0400

    Turn off Android hardware acceleration so that takeScreenshot works.

--- lib/src/bridge_inapp_webview.dart
+++ lib/src/bridge_inapp_webview.dart
@@ -49,6 +49,7 @@ class DsBridgeInAppWebViewState extends State<DsBridgeInAppWebView> {
                   userAgent:
                       "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 DsBridge/1.0.0",
                 ),
+                android: AndroidInAppWebViewOptions(hardwareAcceleration: false),
                 ios: IOSInAppWebViewOptions(allowsInlineMediaPlayback: false)),
           );
           controller.loadFile(
